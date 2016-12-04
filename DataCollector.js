9258ce41ecf7fd5d14a2b66ee33cdbd9a143ce24
Anderson Cruz <aac@icomp.ufam.edu.br>
2016-12-04 19:03:13 -0400

    Transformação PDF em Imagens, Ajustes ID Questionário e DataCollector

--- DataCollector.js
+++ DataCollector.js
@@ -2,11 +2,14 @@ var debug = true;
 var timer;
 var currentScreen = "";
 var idClass = "";
+var ip = "localhost"; 
+var idAluno = "8";
 
 var multioption = {
     status: false,
-    id: "Q1-easy",
+    id: "Q1-hard",
     len: "1",
+    question: ["Q1-hard","Q2-hard","Q3-hard"]
 } 
 
 onload =  function(e){
@@ -25,15 +28,23 @@ function overflowTimer(){
     var timestamp = new Date().getTime();
 
     if (currentScreen == "questionario.html" && idClass == "") idClass = "Q:1:H:1-1";
+    if (idClass == "troca-Q1 btn btn-info btn-lg"){
+        idClass = "Q:1:E:1-1";
+    } else if (idClass == "troca-Q2 btn btn-info btn-lg"){
+        idClass = "Q:2:E:1-2";
+    } else if (idClass == "troca-Q3 btn btn-info btn-lg"){
+        idClass = "Q:3:E:1-3";
+    }
+
     if(debug) {
        /* console.log("-------after two seconds----------");
         console.log("DataCollector timestamp: "+timestamp);
         console.log("Tela Atual: "+currentScreen);*/
     }
 
-    $.post("http://localhost:5000/storage/1",
+    $.post("http://"+ ip +":5000/storage/1",
         {
-            idUser: "2",
+            idUser: idAluno,
             timeStamp: timestamp,
             tipo: null,
             tela: currentScreen,
@@ -46,14 +57,23 @@ function overflowTimer(){
         function(data, status){
             //console.log("PLAYER------Data: " + data + "\nStatus: " + status);
             response = data.recommendation[0].recommendation;
+            questions = data.recommendation[1].questions;
             response = response.split(";");
+            questions = questions.split(",")
             console.log("response: " + response);
-            console.log("response len: " + response.length);
-
+            //console.log("response len: " + response.length);
+            console.log("questions: " + questions);
             if (response.length > 1){
                 multioption.status = true;
                 multioption.length = response.length;
                 multioption.id = response;
+                multioption.question = questions;
+               // console.log("multioption " + multioption.question[1]);
+            } else {
+                multioption.status = false;
+                multioption.length = response.length;
+                multioption.id = response;
+                multioption.question = questions;
             }
         });
 }
@@ -100,10 +120,24 @@ function listenClick(e){
     } else {
         idClass = e.target.className;
     }
+
+
+    if (e.target.className == "Q:1:H:1-1 list-group-item" && multioption.question[0] == "Q1-easy" ) idClass = "Q:1:E:1-1 list-group-item";
+    else if (e.target.className == "Q:2:H:1-2 list-group-item" && multioption.question[1] == "Q2-easy") idClass = "Q:2:E:1-2 list-group-item";
+    else if (e.target.className == "Q:3:H:1-3 list-group-item" && multioption.question[2] == "Q3-easy") idClass = "Q:3:E:1-3 list-group-item";
+    
+    //console.log("IDCLASS: " + idClass);
+
     //idTela = getCookie("screen");
     var timestamp = new Date().getTime();
+    /*var tag = e.target.localName;
+    if (e.target.localName == "input") {
+       // console.log("INPUT");
+        tag = e.target.localName + "-" + e.target.defaultValue;
+        //console.log("INPUT: " + tag);
+    }*/
 
-    if(debug) {
+  /*  if(debug) {
        console.log("-----------------");
         console.log(e);
         console.log("tela:" + currentScreen);
@@ -113,14 +147,15 @@ function listenClick(e){
         console.log("target class: " + e.target.className);
         console.log("selected option:", e.target.defaultValue);
         console.log(e.target.localName);
+      //  console.log(tag);
         console.log(e.timeStamp);
         console.log(e.which);
         console.log("DataCollector timestamp: "+timestamp); 
-    }
+    }*/
 
-    $.post("http://localhost:5000/storage/1",
+    $.post("http://"+ ip +":5000/storage/1",
     {
-      idUser: "2",
+      idUser: idAluno,
       timeStamp: timestamp,
       tipo: e.type,
       tela: currentScreen,
@@ -132,18 +167,26 @@ function listenClick(e){
       classId: idClass
     },
     function(data, status){
-        // alert("Data: " + data + "\nStatus: " + status);
-             //console.log("PLAYER------Data: " + data + "\nStatus: " + status);
             response = data.recommendation[0].recommendation;
+            questions = data.recommendation[1].questions;
             response = response.split(";");
+            questions = questions.split(",")
             console.log("response: " + response);
-            console.log("response len: " + response.length);
+            //console.log("response len: " + response.length);
+            console.log("questions: " + questions);
             if (response.length > 1){
                 multioption.status = true;
                 multioption.length = response.length;
                 multioption.id = response;
+                multioption.question = questions;
+                //console.log("multioption " + multioption.question);
+            } else {
+                multioption.status = false;
+                multioption.length = response.length;
+                multioption.id = response;
+                multioption.question = questions;
             }
-    });
+        });
 }
 
 function listemMouseOver(e){
